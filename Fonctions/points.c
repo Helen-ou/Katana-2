dcf6069c9aadc4588e6d20099621f34aaf01a4e6
Helen-ou <helene.desroches@outlook.fr>
2024-02-13 19:23:27 +0100

    Il faut débugger routine.c et fin.c
    hey

--- Fonctions/points.c
+++ Fonctions/points.c
@@ -1,62 +1,47 @@
 #include <stdio.h>
 
-/*// Va vérifier le nombre de points de chaque joueurs et les afficher
-#include <stdio.h>
-
-// Echanger 2 éléments
-void swap(int *a, int *b)
+void point_calcul(Joueurs *joueurs, int nb_joueurs)
 {
-  int temp = *a;
-  *a = *b;
-  *b = temp;
-}
-
-void selectionSort(int array[], int size)
-{
-  for (int step = 0; step < size - 1; step++)
-  {
-    int min_idx = step;
-    for (int i = step + 1; i < size; i++)
+    int multiplicateurs[5][4] = {{2, 0, 1, 0}, {1, 2, 1.5, 0}, {1, 1, 1, 2}, {1, 2, 1, 3}, {1, 1, 1, 3}};
+    char nom_factions[4][20] = {"Shogun", "Samouraï", "Ninja", "Ronin"};
+    int points_factions[4] = {0, 0, 0, 0};
+    // Créer le tableau points_factions avec les points de chaque faction
+    for (int i = 0; i < nb_joueurs; i++)
     {
-      if (array[i] < array[min_idx])
-        min_idx = i;
+        switch (joueurs[i].classe)
+        {
+        case 0:
+            points_factions[0] = joueurs[i].honneur * multiplicateurs[nb_joueurs - 3][0] break;
+        case 1:
+            points_factions[1] += joueurs[i].honneur * multiplicateurs[nb_joueurs - 3][1] break;
+        case 3:
+            points_factions[2] += joueurs[i].honneur * multiplicateurs[nb_joueurs - 3][2] break;
+        case 4:
+            points_factions[3] += joueurs[i].honneur * multiplicateurs[nb_joueurs - 3][3] break;
+        }
     }
-    swap(&array[min_idx], &array[step]);
-  }
-}
-
-void printArray(int array[], int size)
-{
-  for (int i = 0; i < size; ++i)
-  {
-    printf("%d par le joueur %d", array[i]);
-  }
-  printf("\n");
-}
-
-// Fonction à exécuter
-void point_calcul(Joueurs *joueurs, int nb_joueurs)
-{
-  int i;
-  int tableau_p[nb_joueurs]; // tableau points
-
-  for (i; i < nb_joueurs; i++)
-  { // On met les points dans un tableau
-    tableau_p[i] = joueurs[i].points;
-  }
-
-  int taille = sizeof(tableau_p) / sizeof(tableau_p[0]);
-  selectionSort(tableau_p, taille);
-  printf("Les résultats des points de la partie sont : \n");
-  printArray(tableau_p, taille);
-}*/
-
-void point_calcul(Joueurs *joueurs, int nb_joueurs)
-{
-  int multiplicateurs[5][4] = {{2, 0, 1, 0}, {1, 2, 1.5, 0}, {1, 1, 1, 2}, {1, 2, 1, 3}, {1, 1, 1, 3}};
-  int points_factions[4];
-  for (int i = 0; i < nb_joueurs; i++)
-  {
-      
-  }
+    // Afficher l'équipe victorieuse et les points
+    int tableau_trie[4][2]; // {points_max, id_max}
+    int points_max = 0;
+    int id_max = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (points_max < points_factions[i])
+            {
+                points_max = points_factions[i];
+                id_max = i;
+            }
+        }
+        tableau_trie[i][0] = points_max;
+        tableau_trie[i][1] = id_max;
+        points_factions[id_max] = -100;
+        points_max = 0;
+        id_max = 0;
+    }
+    printf("La faction gagnante est %s avec %d points, félicitations !", nom_factions[tableau_trie[0][1]], tableau_trie[0][0]);
+    printf("La deuxième faction est %s avec %d points, félicitations !", nom_factions[tableau_trie[1][1]], tableau_trie[0][0]);
+    printf("La troisième faction est %s avec %d points, félicitations !", nom_factions[tableau_trie[2][1]], tableau_trie[0][0]);
+    printf("La quatrième faction est %s avec %d points, félicitations !", nom_factions[tableau_trie[3][1]], tableau_trie[0][0]);
 }
